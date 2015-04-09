30d817bba77067ba25f5970c31e92753b98ab17f
OzzyVenie <ethanslh@gmail.com>
2015-04-09 13:46:59 +1000

    Added animations for walking

--- Scripts/player.js
+++ Scripts/player.js
@@ -1,8 +1,45 @@
-
+var LEFT = 0;
+var RIGHT = 1;
+var ANIM_IDLE_LEFT = 0;
+var ANIM_JUMP_LEFT = 1;
+var ANIM_WALK_LEFT = 2;
+var ANIM_SHOOT_LEFT = 3;
+var ANIM_CLIMB = 4;
+var ANIM_IDLE_RIGHT = 3;
+var ANIM_JUMP_RIGHT = 4;
+var ANIM_WALK_RIGHT = 5;
+var ANIM_SHOOT_RIGHT = 8;
+var ANIM_MAX = 9;
 
 var Player = function()
 {
-	this.image = document.createElement("img");
+	this.sprite = new Sprite("./Images/ChuckNorris.png");
+	
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+	[0, 1, 2, 3, 4, 5, 6, 7]); //LEFT_IDLE = 0
+	
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+	[8, 9, 10, 11, 12]);//LEFT_JUMP = 1
+	
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+	[13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]); //LEFT_WALK = 2 
+	
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+	[52, 53, 54, 55, 56, 57, 58, 59]); //RIGHT_IDLE = 3
+	
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+	[60, 61, 62, 63, 64]);//RIGHT_JUMP = 4
+	
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+	[65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]);//RIGHT_WALK = 5
+	
+	
+	
+	for(var i=0; i<6; i++)
+	{
+		this.sprite.setAnimationOffset(i, -55, -87);
+	}
+
 	
 	this.position = new Vector2();
 	this.position.set(100, 500);
@@ -14,15 +51,16 @@ var Player = function()
 	this.jumping = false;
 	this.falling = false;
 	
-	this.flipPlayer = false;
+	this.dierection = LEFT;
 	
 	this.angularVelocity = 0;
 	this.rotation = 0;
-	this.image.src = "Images/Lunk.png";
 };
 
 Player.prototype.update = function(deltaTime)
 {
+	this.sprite.update(deltaTime);
+
 	var acceleration = new Vector2();
 	var playerAccel = 6000;
 	var playerDrag = 12;
@@ -31,15 +69,51 @@ Player.prototype.update = function(deltaTime)
 	
 	acceleration.y = playerGravity;
 	
-	if ( keyboard.isKeyDown(keyboard.KEY_A) )
+	if(keyboard.isKeyDown(keyboard.KEY_A) == true )
 	{
 		acceleration.x -= playerAccel;
-		this.flipPlayer = false;
+		left = true;
+		this.direction = LEFT;
+		if(this.sprite.currentAnimation != ANIM_WALK_LEFT && this.jumping == false && this.falling == false)
+			this.sprite.setAnimation(ANIM_WALK_LEFT);
 	}
-	if ( keyboard.isKeyDown(keyboard.KEY_D) )
+
+	else if(keyboard.isKeyDown(keyboard.KEY_D) == true )
 	{
 		acceleration.x += playerAccel;
-		this.flipPlayer = true;
+		right = true;
+		this.direction = RIGHT;
+		if(this.sprite.currentAnimation != ANIM_WALK_RIGHT && this.jumping == false && this.falling == false)
+			this.sprite.setAnimation(ANIM_WALK_RIGHT);
+	}
+	else
+	{
+		if(this.jumping == false && this.falling == false)
+		{
+			if(this.direction == LEFT)
+			{
+				if(this.sprite.currentAnimation != ANIM_IDLE_LEFT)
+				this.sprite.setAnimation(ANIM_IDLE_LEFT);
+			}
+			else
+			{
+				if(this.sprite.currentAnimation != ANIM_IDLE_RIGHT)
+				this.sprite.setAnimation(ANIM_IDLE_RIGHT);
+			}
+		}
+	}
+	if(this.jumping || this.falling )
+	{
+		if(this.direction == LEFT)
+		{
+			if(this.sprite.currentAnimation != ANIM_JUMP_LEFT)
+			this.sprite.setAnimation(ANIM_JUMP_LEFT);
+		}
+		else
+		{
+			if(this.sprite.currentAnimation != ANIM_JUMP_RIGHT)
+			this.sprite.setAnimation(ANIM_JUMP_RIGHT);
+		}
 	}
 	
 	if ( this.velocity.y > 0 )
@@ -51,13 +125,17 @@ Player.prototype.update = function(deltaTime)
 		this.falling = false;
 	}
 	
-	if ( keyboard.isKeyDown(keyboard.KEY_SPACE) && !this.jumping && !this.falling )
+	if ( keyboard.isKeyDown(keyboard.KEY_W) && !this.jumping && !this.falling )
 	{
 		acceleration.y -= jumpForce;
 		this.jumping = true;
+		if(this.direction == LEFT)
+			this.sprite.setAnimation(ANIM_JUMP_LEFT)
+		else
+			this.sprite.setAnimation(ANIM_JUMP_RIGHT)
 	}
 	
-	
+
 	
 	var dragVector = this.velocity.multiplyScalar(playerDrag);
 	dragVector.y = 0;
@@ -67,8 +145,7 @@ Player.prototype.update = function(deltaTime)
 	this.position = this.position.add(this.velocity.multiplyScalar(deltaTime));
 	
 	var tx = pixelToTile(this.position.x);
-	var ty = pixelToTile(this.position.y);
-	
+	var ty = pixelToTile(this.position.y);	
 	var nx = this.position.x % TILE;
 	var ny = this.position.y % TILE;
 	
@@ -123,19 +200,7 @@ Player.prototype.update = function(deltaTime)
 	}
 }
 
-Player.prototype.draw = function(){
-	if(this.flipPlayer == true){
-		ctx.save();
-			ctx.translate(this.width, 0);
-			ctx.translate(this.position.x, this.position.y);
-			ctx.scale(-1, 1);
-			ctx.drawImage(this.image, +this.width/2, -this.height + TILE);
-		ctx.restore();
-	}else{
-		ctx.save();
-			ctx.translate(this.position.x, this.position.y);
-			ctx.rotate(this.rotation);
-			ctx.drawImage(this.image, -this.width/2 + TILE, -this.height + TILE);
-		ctx.restore();
-	}
+Player.prototype.draw = function()
+{
+	this.sprite.draw(ctx, this.position.x, this.position.y);
 }
